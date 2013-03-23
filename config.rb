35d2e5d8ea0c0d25c3d96196644c655eed5bbdfe
Chris Bell <cjbell1988@gmail.com>
2013-03-23 18:52:15 +0000

    Initial

---
+++ config.rb
@@ -0,0 +1,42 @@
+###
+# Compass
+###
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy (fake) files
+proxy "/2013/04/brew-the-perfect-coffee.html", 'brew-the-perfect-coffee.html'
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  activate :cache_buster
+
+  # Use relative URLs
+  activate :relative_assets
+end
